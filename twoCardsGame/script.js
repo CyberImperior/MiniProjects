89e626ef85a0491180e70422daffd7a5bee271fb
Ruslan Mingazov <mingazay@gmail.com>
2022-04-08 17:48:15 +0300

    first work

--- twoCardsGame/script.js
+++ twoCardsGame/script.js
@@ -1,111 +1,106 @@
-const arrayOfCards = [];
+window.addEventListener('DOMContentLoaded', () => {
 
-// задавать селекторами надо
+  const arrayOfCards = [];
+  const parentCards = document.querySelector('.container-cards-game');
+  makeDivsForGame();
+  makeArrForGame();
+  const divs = document.querySelectorAll('.cards');
+  contributeJpgsInDivs();
 
-const cardsSt = document.querySelector('.oneCard');
-const parentCards = document.querySelector('.container-cards-game');
-makeDivsForGame();
-makeArrForGame();
-const divs = document.querySelectorAll('.cards');
-contributeJpgsInDivs();
+  console.log(divs);
 
-console.log(divs);
+  function closeAll() {
+    divs.forEach((item) => {
+      if (!item.classList.contains('right')) {
+        item.classList.add('back');
+      }
+    })
+  }
 
-function openTwoCards(amount = 7) {
-  const back = document.querySelectorAll('.back');
-    if (back.length == ((amount*2) - 3)) {
-      closeAll();
+  function makeArrForGame(amount = 7, blancArr = arrayOfCards) {
+    for (let i = 0; i <= (amount - 1); i++) {
+      blancArr.push(i + 1);
+      blancArr.push(i + 1);
     }
-}
+    shuffle(blancArr);
+    return blancArr;
+  }
 
-function closeAll() {
-  divs.forEach((item, i) => {
-    divs[i].classList.add('back');
+  function makeDivsForGame(amount = 7) {
+    for (let i = 0; i <= ((amount * 2 - 1)); i++) {
+      let iDiv = document.createElement('div');
+      iDiv.classList.add('cards')
+      parentCards.append(iDiv);
+
+    }
+  }
+
+  function contributeJpgsInDivs(blancArr = arrayOfCards) {
+    divs.forEach((itemDivs, i) => {
+      itemDivs.style.cssText = `background: url("img/${blancArr[i]}.jpg") no-repeat; background-size: cover;`;
+      itemDivs.classList.add(blancArr[i]);
+      itemDivs.classList.add('back')
+    })
+  }
+
+  function check2Cards() {
+    divs.forEach((item) => {
+      count = 0
+      if (!item.contains('back')) {
+        count++;
+        if (count = 2) {
+          return true
+        } else {
+          return false
+        }
+      }
+    })
+  }
+
+  function is2CardTrue() {
+    if (divs.classList.item(1)) {
+
+    }
+  }
+
+  function mageOneGameData(arrCountCards) {
+
+    arrCountCards.forEach((item, i) => {
+      console.log(item);
+    });
+  }
+
+  const arrEqualClass = [];
+  let lastEvent
+  //через эвент надо делать логику тоже самое что if click
+  parentCards.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target && target.classList.contains('back')) {
+      arrEqualClass.push(target.classList.item(1));
+
+      console.log(arrEqualClass.length)
+      if (arrEqualClass.length == 1 || arrEqualClass.length == 2) {
+        target.classList.remove('back')
+
+        if (arrEqualClass[0] === arrEqualClass[1]) {
+          
+          console.log('y')
+          divs.forEach(item => {
+            if (!item.classList.contains('back'))
+              item.classList.add('right')
+          })
+        }
+      } else {
+        arrEqualClass.splice(0, 2)
+        closeAll()
+        target.classList.remove('back')
+      }
+    }
   })
-}
-
-function makeArrForGame(amount = 7, blancArr = arrayOfCards) {
-  for (let i = 0; i <= (amount - 1); i++) {
-    blancArr.push(i+1);
-    blancArr.push(i+1);
-  }
-  shuffle(blancArr);
-  return blancArr;
-}
-
-function makeDivsForGame(amount = 7) {
-  for (let i = 0; i <= ((amount * 2 - 1)); i++) {
-    let iDiv = document.createElement('div');
-    iDiv.classList.add('cards', 'back')
-    parentCards.append(iDiv);
-
-  }
-}
-
-function contributeJpgsInDivs(blancArr = arrayOfCards) {
-  divs.forEach((item, i) =>{
-    divs[i].style.cssText = `background: url("img/${blancArr[i]}.jpg") no-repeat; background-size: cover;`;
-    divs[i].classList.add(blancArr[i]);
-
-  })
-}
-
-// function makeClassForClones(blancArr = arrayOfCards) {
-//   blancArr.forEach((itemAr, iAr) => {
-//   divs.forEach((item, i) => {
-//       if ((i+1) == itemAr) {
-//         divs[i].classList.add(i);
-//       }
-//     })
-//   })
-// }
-
-function checkCard() {
-
-}
-
-function mageOneGameData(arrCountCards) {
-
-  arrCountCards.forEach((item, i) => {
-    console.log(item);
-  });
-
-}
-
-const arrEqualClass = [];
-//через эвент надо делать логику
-parentCards.addEventListener('click', event => {
-  const target = event.target;
-
-  if (arrEqualClass.length < 2) {
-    arrEqualClass.slice(0,2);
-  }
-
-  if (target && target.classList.contains('back')) {
-    target.classList.remove('back');
-
-  }
-  else if (target && target.classList.contains('cards')) {
-    target.classList.add('back');
-  }
-  openTwoCards();
-  arrEqualClass.push(target.classList.item(1));
-  console.log(arrEqualClass)
-})
-
-console.log(cardsSt)
 
 
-
-
-
-
-
-
-
-
-
-function shuffle(array) {
+  function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
 
@@ -117,3 +112,4 @@ function shuffle(array) {
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
+});
